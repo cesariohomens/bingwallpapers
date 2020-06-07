bb6f6501bf74bc1aeb2436504b43d29d83abe30f
Cesário Garcia Mil-Homens <39047211+cesariohomens@users.noreply.github.com>
2020-06-07 18:46:50 +0100

    Update bingwallpapers.sh

--- bingwallpapers.sh
+++ bingwallpapers.sh
@@ -8,10 +8,12 @@
 # 1. Execute this script the first time and set "bingwallpaper.jpg" as wallpaper and lockscreen
 # 2. Config $wallpaperPath (line 13 of this script) for the full path of where you want "bingwallpaper.jpg" to be (it should be on the user Pictures folder)
 # 3. On the terminal execute the following command, this will check for new updates every two hours
-# crontab -l | { cat; echo "0 */2 * * * /bin/bash /home/username/scripts/bingwallpapers.sh >/dev/null 2>&1"; } | crontab -
+# crontab -e
+# Insert the following code -> "0 */2 * * * /bin/bash /home/username/scripts/bingwallpapers.sh >/dev/null 2>&1"
 
 ## Config wallpaper path
-wallpaperPath="/home/cesario/Pictures"
+wallpaperPath="/home/user/Pictures/Wallpapers"
+backgroundPath="/home/user/Programs"
 
 ## Do not edit anything bellow
 wget -q --spider http://google.com
@@ -29,7 +31,20 @@ if [ $? -eq 0 ]; then
 		else
 			echo "$fileName not found."
 			wget "https://www.bing.com/th?id="$fileName -O $wallpaperPath/$fileName
-			cp $wallpaperPath"/"$fileName $wallpaperPath"/"bingwallpaper.jpg
+			cp $wallpaperPath"/"$fileName $backgroundPath"/background.jpg"
+			
+			# The following code is to change KDE wallpaper
+			dbus-send --session --dest=org.kde.plasmashell --type=method_call /PlasmaShell org.kde.PlasmaShell.evaluateScript 'string:
+            var Desktops = desktops();                                                                                                                       
+            for (i=0;i<Desktops.length;i++) {
+                    d = Desktops[i];
+                    d.wallpaperPlugin = "org.kde.image";
+                    d.currentConfigGroup = Array("Wallpaper",
+                                                "org.kde.image",
+                                                "General");
+                    d.writeConfig("Image", "'$backgroundPath'/background.jpg");
+            }'
+			
 		fi
 else
     echo "Offline"
